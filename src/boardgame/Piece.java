92e46ebd635e141160f4b2f457326f4764778560
Alysson Almeida <alysson-am@hotmail.com>
2020-07-08 16:25:55 +1000

    Handling exceptions and clearing screen

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -7,12 +7,12 @@ public class Piece {
 	
 	
 	public Piece(Board board) {
-		
+		//posicao da peca criada e null pq nao foi colocada no board ainda
 		this.board = board;
 		position =  null;
 	}
 
-
+	// only the boardlayer package and subclasses can access the method -  internal use 
 	protected Board getBoard() {
 		return board;
 	}
