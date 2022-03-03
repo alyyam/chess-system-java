e42e585ca1d7d35a715d3851c4846a1952545d8e
Alysson Machado <alysson-am@hotmail.com>
2022-03-03 17:22:41 +1100

    First class : Position

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Position position = new Position(5,4);
+		
+		
+		System.out.println(position);
+
+	}
+
+}
